96bc76477eed449ffed24b17d1b49886d5d6247f
Yashaswini1308 <yashaswiniputta09@gmail.com>
2025-05-01 17:04:38 -0500

    Final Commit

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 
 const connect = async () => {
     try{
-        const connect = await mongoose.connect("mongodb://localhost:27017/schoolbus");
+        const connect = await mongoose.connect("mongodb+srv://tracker:tracker123@cluster0.xp7ut.mongodb.net/schoolbus?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Database connected : ",connect.connection.name);
     } catch (err) {
         console.log("Unable to connect database due to Error : ",err);
